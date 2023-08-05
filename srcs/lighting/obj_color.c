6b2951bd60aacfa7011647f2995191205657795d
laian <tanlaian2004@gmail.com>
2023-08-06 00:59:29 +0800

    feat: started plane uv (not working)

--- srcs/lighting/obj_color.c
+++ srcs/lighting/obj_color.c
@@ -38,6 +38,46 @@ void	calculate_uv_circle(t_ray *ray, t_object *o, double store[2])
 	// printf("(%f %f)\n", store[0], store[1]);
 }
 
+void	calculate_uv_plane(t_ray *ray, t_object *o, double store[2])
+{
+	t_vec3	*zeroed;
+
+	t_vec3	*temp;
+
+	t_vec3	*e1;
+	t_vec3	*e2;
+
+	zeroed = vec3_difference(ray->pos_vector, o->ob_coords, O_CREATE);
+	vec3_normalize(zeroed, O_REPLACE);
+
+	temp = vec3_init(1, 0, 0);
+
+	e1 = vec3_crossproduct(o->ob_planes->pl_vec_normal, temp, O_CREATE);
+	vec3_normalize(e1, O_REPLACE);
+
+	if (e1->raw_matrix->m[0][0] == 0 && e1->raw_matrix->m[1][0] == 0 && e1->raw_matrix->m[2][0] == 0)
+	{
+		vec3_free(&e1);
+		vec3_free(&temp);
+		temp = vec3_init(0, 0, 1);
+		e1 = vec3_crossproduct(o->ob_planes->pl_vec_normal, temp, O_CREATE);
+		vec3_normalize(e1, O_REPLACE);
+	}
+
+	e2 = vec3_crossproduct(o->ob_planes->pl_vec_normal, e1, O_CREATE);
+	vec3_normalize(e2, O_REPLACE);
+
+	store[0] = vec3_dotproduct(e1, zeroed);
+	store[1] = vec3_dotproduct(e2, zeroed);
+
+	vec3_free(&zeroed);
+	vec3_free(&temp);
+	vec3_free(&e1);
+	vec3_free(&e2);
+
+	// printf("(%f %f)\n", store[0], store[1]);
+}
+
 t_vec3	*get_object_color(t_ray *ray, t_object *o)
 {
 	// im gonna regret this later, watch
@@ -50,6 +90,11 @@ t_vec3	*get_object_color(t_ray *ray, t_object *o)
 	}
 	else
 	{
+		if (o->ob_type == PLANE)
+		{
+			calculate_uv_plane(ray, o, uv_coord);
+			return (get_rgb(o->ob_texture, uv_coord[0], uv_coord[1]));
+		}
 		if (o->ob_type == CIRCLE)
 		{
 			// printf("ray in goc value = ");
