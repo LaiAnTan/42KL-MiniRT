4349c14921abf2ad002bdf279808b8081b8b8888
tlai-an <tlai-an@student.42kl.edu.my>
2023-09-05 14:32:23 +0800

    chore: rename clean_exit

---
+++ srcs/clean_exit.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clean_exit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlai-an <tlai-an@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/05 14:32:00 by tlai-an           #+#    #+#             */
+/*   Updated: 2023/09/05 14:32:01 by tlai-an          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/minirt.h"
+
+void	destory_images(t_mlx_info *mlx)
+{
+	if (mlx->img[0].img != NULL)
+	{
+		mlx_destroy_image(mlx->mlx, mlx->img[0].img);
+		mlx->img[0].img = NULL;
+	}
+	if (mlx->img[1].img != NULL)
+	{
+		mlx_destroy_image(mlx->mlx, mlx->img[1].img);
+		mlx->img[1].img = NULL;
+	}
+}
+
+void	clean_exit(t_data *data)
+{
+	if (data->mlx->mlx_win)
+		mlx_destroy_window(data->mlx->mlx, data->mlx->mlx_win);
+	destory_images(data->mlx);
+	data->scene = scene_free(data->scene, data->mlx->mlx);
+	exit(0);
+}
